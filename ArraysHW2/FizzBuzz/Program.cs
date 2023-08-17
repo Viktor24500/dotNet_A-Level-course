341ecb4855410fd8d798462998e6fac2b1c9496d
ViktorPodgornyy <viktor.podgornyy1@gmail.com>
2023-08-17 23:35:46 +0300

    HW2 (arrays, if/else if)

---
+++ ArraysHW2/FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FizzBuzz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberGenerator();
+            Console.ReadKey();
+        }
+        public static void NumberGenerator()
+        {
+            int number = 0;
+            while (number <= 100)
+            {
+                ifConditionTrue(number);
+                number++;
+            }
+        }
+        public static void ifConditionTrue(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine($"Number - {number} - FizzBuzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine($"Number - {number} - Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine($"Number - {number} - Buzz");
+            }
+            else
+            {
+                Console.WriteLine($"Number - {number}");
+            }
+        }
+    }
+}
---
+++ ArraysHW2/HW_Task2/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace HW2_Task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 20;
+            int counter = 0;
+            int[] startArray = new int[length];
+            //int[] finalArray = new int[length];
+            startArray = RandomArray(startArray, length);
+            for (int i = 0; i < length; i++)
+            {
+                if (startArray[i] <= 888)
+                {
+                    counter++;
+                }
+            }
+            int[] finalArray = new int[counter];
+            finalArray = CopyArray(startArray, finalArray);
+            //finalArray =SortArray(finalArray);
+            Console.WriteLine("old array");
+            PrintArray(startArray);
+            Console.WriteLine();
+            Console.WriteLine("new array after");
+            PrintArray(finalArray);
+            Console.ReadKey();
+        }
+        public static int[] RandomArray(int[] myArray, int length)
+        {
+            Random random = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                myArray[i] = random.Next(2000);
+            }
+            return myArray;
+        }
+        public static void PrintArray(int[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine($"Array {i} --   {myArray[i]}");
+            }
+        }
+        public static int[] CopyArray(int[] startArray, int[] finalArray)
+        {
+            int j = 0;
+            for (int i = 0; i < startArray.Length; i++)
+            {
+                if (startArray[i] <= 888)
+                {
+                    finalArray[j] = startArray[i];
+                    j++;
+                }
+            }
+            ////Length finalArray=20
+            //for (int i = 0; i < startArray.Length; i++)
+            //{
+            //    if (startArray[i] <= 888)
+            //    {
+            //        finalArray[i] = startArray[i];
+            //    }
+            //    else
+            //    {
+            //        finalArray[i] = 0;
+            //    }
+            //}
+            Array.Sort(finalArray);
+            Array.Reverse(finalArray);
+            return finalArray;
+        }
+    }
+}
