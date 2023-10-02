a9ad5ee7fdadbd782530ed4da9a56c957fce3a7a
ViktorPodgornyy <viktor.podgornyy1@gmail.com>
2023-10-02 11:56:15 +0300

    add Linq

---
+++ LinqEvents/Linq/Linq/Linq1_12.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public class Linq1_12
+    {
+        public static void LinqFrom1To12() 
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 100, -10, 22, 36, 893, 13, 104, 33, 1, 3, -10, 22, 22, 36, 13, 1, 2 };
+            List<string> strings = new List<string>() {"Abilities", "forfeited", "situation",
+                "extremely", "my", "to", "he", "resembled", "Old", "had", "conviction",
+                "discretion", "understood", "put", "principles", "you" };
+            string str = "Abilities forfeited situation extremely my to he resembled." +
+                " Old had conviction discretion understood put principles you.";
+
+            var querry1 = numbers.Where(x => x%2==0 || x%3==0);
+            var querry2 = numbers.Sum();
+            var querry3 = numbers.Average();
+            var querry4 = numbers.Max();
+            var querry5 = numbers.Min();
+            var querry6 = numbers.Where(x => x>10).Select(x=> x*10);
+            var querry7 = str.Distinct();
+            var querry8 = numbers.GroupBy(x => x).Select(group => new {x=group.Key, frequence = group.Count()});
+            var querry9Even = numbers.Select(x => x % 2 == 0);
+            var querry9Odd = numbers.Select(x => x % 2 == 1);
+            Console.WriteLine($"Max in Even = {querry9Even.Max()}, max in Odd = {querry9Odd.Max()}");
+            var querry10 = numbers.Select(x => x>numbers.Average());
+            var querry11 = strings.GroupBy(x => x.Length);
+            string subString = Console.ReadLine();
+            var querry12 = strings.Where(str => str.Contains(subString, StringComparison.OrdinalIgnoreCase)).
+                OrderBy(str => str.Length).Select(str => $"{char.ToUpper(str[0])}{str.Substring(1).ToLower()}");
+            Console.WriteLine(querry1);
+        }
+    }
+}
---
+++ LinqEvents/Linq/Linq/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Linq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<User> listUsers = new List<User>()
+            {
+                new User("Ava", "Smith", "ava.smith@gmail.com", new DateTime(1990, 7, 14), 1),
+                new User("Artem", "Johnson", "artem.johnson@costoso.com", new DateTime(2007, 9, 20), 2),
+                new User("Nika", "Williams", "nika.williams@ukr.net", new DateTime(2001, 8, 10), 3),
+                new User("Alvin", "Rodriguez", "alvin.rodriguez@outlook.com", new DateTime(2010, 7, 25), 4),
+                new User("John", "Martinez", "john.martinez@yahoo.com", new DateTime(2018, 12, 31), 5),
+            };
+
+            //1
+            var users = listUsers.Where(x => 18 < DateTime.Now.Year - x.DateOfBirth.Year).
+                Select(x => new {firstName = x.FirstName, lastName = x.LastName, dateOfBirth=x.DateOfBirth,
+                    age = DateTime.Now.Year - x.DateOfBirth.Year });
+
+            foreach (var item in users) 
+            {
+                Console.WriteLine($"{item.firstName}, {item.lastName}, date of birh - {item.dateOfBirth}, full years - {item.age}");
+            }
+
+            //2
+            var groupUsersForMail = listUsers.GroupBy(x => x.Email.Contains("gmail.com")).
+                Select(group => new
+                {
+                    domain = group.Key,
+                    count = group.Count()
+                });
+
+            //3
+            Dictionary<int, User> userDict = listUsers.ToDictionary(x => x.Id);
+
+            foreach (KeyValuePair<int, User> kvp in userDict)
+            {
+                Console.WriteLine(
+                    "Key {0}: {1}, {2}, {3}, {4}",
+                    kvp.Key,
+                    kvp.Value.FirstName,
+                    kvp.Value.LastName,
+                    kvp.Value.Email,
+                    kvp.Value.DateOfBirth);
+            }
+
+            //4
+            var groupUsersForLastName = listUsers.GroupBy(x => x.LastName).
+                Select(group => new { firstName = group.First().FirstName,
+                DateOfBirth = group.First().DateOfBirth}).
+                OrderBy(group => group.DateOfBirth).ToList();
+
+
+            Console.ReadKey();
+        }
+    }
+}
