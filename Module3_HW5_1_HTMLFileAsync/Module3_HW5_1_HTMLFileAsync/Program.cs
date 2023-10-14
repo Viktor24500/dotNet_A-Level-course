4e5750828bba9c032f46d450151d424904c29fb8
ViktorPodgornyy <viktor.podgornyy1@gmail.com>
2023-10-15 00:07:11 +0300

    Module3 HW5 Task 2

---
+++ Module3_HW5_1_HTMLFileAsync/Module3_HW5_1_HTMLFileAsync/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Module3_HW5_1_HTMLFileAsync
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Input your urls with space");
+            string[] urls = Console.ReadLine().Split(' ');
+
+            SiteWordCounterAsync siteWordCounterAsync = new SiteWordCounterAsync();
+            await siteWordCounterAsync.ProcessWithSiteAsync(urls);
+
+            Console.WriteLine("amount of words");
+            foreach (var kvp in siteWordCounterAsync.SiteWords)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+        
+        }
+    }
+}
---
+++ Module3_HW5_1_HTMLFileAsync/Module3_HW5_1_HTMLFileAsync/SiteWordCounterAsync.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_HW5_1_HTMLFileAsync
+{
+    public class SiteWordCounterAsync
+    {
+        public Dictionary<string, int> SiteWords { get; } = new Dictionary<string, int>();
+
+        public async Task ProcessWithSiteAsync(string[] urls) 
+        {
+            SiteDownloader siteDownloader = new SiteDownloader();
+            FileRead fileRead = new FileRead();
+            WordCounter wordCounter = new WordCounter();
+            WordSplitter wordSplitter = new WordSplitter();
+            foreach (var url in urls) 
+            {
+                string normalizeUrl = siteDownloader.NormalizeUrl(url);
+                string html = await siteDownloader.SiteDownload(url);
+                if (!string.IsNullOrEmpty(html))
+                {
+                    var words = wordSplitter.WordSplit(html);
+                    var uniqueWord = words.Distinct().ToList();
+                    SiteWords[normalizeUrl] = uniqueWord.Count();
+
+                    Console.WriteLine($"Amount of unique symbols {normalizeUrl} - {uniqueWord.Count}");
+
+                    var wordCounts = wordCounter.countWord(words);
+                    var sortedWords = uniqueWord.OrderByDescending(word => wordCounts[word]).ToList();
+                    Console.WriteLine(string.Join("\n", sortedWords.Select(word => $"{word} - {wordCounts[word]}")));
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
