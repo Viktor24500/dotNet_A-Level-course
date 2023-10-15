46d1ec831b1e3ce5c6466d96133840680d36ee75
ViktorPodgornyy <viktor.podgornyy1@gmail.com>
2023-10-15 18:46:14 +0300

    add factorial, fibonacci, async method

---
+++ Module3_HW5_Task2_MultyThread/Module3_HW5_Task2_MultyThread/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Module3_HW5_Task2_MultyThread
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input first num");
+            int.TryParse(Console.ReadLine(), out int firstNum);
+            Console.WriteLine("Input second num");
+            int.TryParse(Console.ReadLine(), out int secondNum);
+
+            Task<int> factorial = Task.Run(() => Factorial.FactorialRecursion(firstNum));
+            Task<int> fibonacci = Task.Run(() => Fibonacci.FibonacciMethod(secondNum));
+
+            Task.WaitAll(factorial, fibonacci);
+            var results = new List<(int Number, int Result)>
+            {
+                (firstNum, factorial.Result),
+                (secondNum, fibonacci.Result)
+            };
+            Console.WriteLine("factorial");
+            Console.WriteLine($"Number: {results[0].Number}, Result: {results[0].Result}");
+            Console.WriteLine("fibonacci");
+            Console.WriteLine($"Number: {results[1].Number}, Result: {results[1].Result}");
+            Console.ReadKey();
+        }
+    }
+}
