f198ab2f03519e27c707f2dbc0724feadf1bf535
Viktor Pidhornyy <viktor.podgornyy1@gmail.com>
2023-12-30 12:47:04 +0200

    add Test class

--- eShop-Sample3/Catalog/Catalog.Host/Services/Interfaces/ICatalogBrandService.cs
+++ eShop-Sample3/Catalog/Catalog.Host/Services/Interfaces/ICatalogBrandService.cs
@@ -5,7 +5,7 @@ namespace Catalog.Host.Services.Interfaces
 {
     public interface ICatalogBrandService
     {
-        //Task<int?> Add(AddCatalogItemRequest addCatalogItem);
+        // Task<int?> Add(AddCatalogItemRequest addCatalogItem);
         Task<PaginatedItems<CatalogBrandDto>> GetByPageAsyncHttpGet(int pageIndex, int pageSize);
     }
 }
---
+++ eShop-Sample3/Tests/Tests/TestCatalogService.cs
@@ -0,0 +1,13 @@
+namespace Tests
+{
+    [TestClass]
+    public class TestCatalogService
+    {
+        [TestMethod]
+        public void TestGetCatalogItem()
+        {
+            int pageSize = 10;
+            int pageIndex = 0;
+        }
+    }
+}
