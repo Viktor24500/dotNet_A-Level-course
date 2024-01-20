19ecd3d0d0a01b9d1fd9ad1fe4a29375b8092f32
Viktor Pidhornyy <viktor.podgornyy1@gmail.com>
2024-01-20 17:07:00 +0200

    add BFF and API controllers

---
+++ eShop-Sample6/Basket/Controllers/ItemController.cs
@@ -0,0 +1,42 @@
+﻿using Basket.Models;
+using Basket.Service.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Policy = "basket.basketitem")]
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemService _itemService;
+
+        public ItemController(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return _itemService.Get();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ItemRequest itemRequest)
+        {
+            var itemToAdd = new Item
+            {
+                Title = itemRequest.Title,
+                Description = itemRequest.Description,
+                Price = itemRequest.Price
+            };
+
+            _itemService.Add(itemToAdd);
+
+            return Ok(itemToAdd.Id);
+        }
+
+    }
+}
---
+++ eShop-Sample6/Basket/Service/ItemService.cs
@@ -0,0 +1,25 @@
+﻿using Basket.Models;
+using Basket.Service.Interface;
+
+namespace Basket.Service
+{
+    public class ItemService : IItemService
+    {
+        private static readonly List<Item> _items = new List<Item>()
+        {
+             new Item() { Title = "Item1", Description = "1", Price = 10.0m },
+             new Item() { Title = "Item2", Description = "2", Price = 20.0m }
+        };
+
+        public List<Item> Get()
+        {
+            return _items;
+        }
+
+        public int Add(Item item)
+        {
+            _items.Add(item);
+            return item.Id;
+        }
+    }
+}
